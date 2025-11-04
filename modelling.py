8c0076c40a03c2bbea7e6367014e35f1dc9d8e85
SymphonyArt <117836194+SymphonyArt@users.noreply.github.com>
2025-11-04 15:22:59 +0700

    final

--- modelling.py
+++ modelling.py
@@ -2,18 +2,20 @@ import mlflow
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import random
-import numpy as np
 import os
+import numpy as np
 import warnings
 import sys
- 
+
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
- 
+
+    # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
     file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "train.csv")
     data = pd.read_csv(file_path)
+
+    # The predicted column is "quality" which is a scalar from [3, 9]
     X_train, X_test, y_train, y_test = train_test_split(
     data.drop("Credit_Score", axis=1),
     data["Credit_Score"],
@@ -23,13 +25,12 @@ if __name__ == "__main__":
     input_example = X_train[0:5]
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
- 
     with mlflow.start_run():
         model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
         model.fit(X_train, y_train)
- 
+
         predicted_qualities = model.predict(X_test)
- 
+
         mlflow.sklearn.log_model(
         sk_model=model,
         artifact_path="model",
